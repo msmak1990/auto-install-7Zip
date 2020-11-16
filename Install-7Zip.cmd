a7af12df40f5d7ab774f6d37502b63da969c3db7
Sukri Kadir <msmak1990@gmail.com>
2020-11-16 13:20:41 +0800

    Clean codes.

--- Install-7Zip.cmd
+++ Install-7Zip.cmd
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 @echo off
 rem 2020-11-14 Sukri Created.
 
@@ -12,19 +11,4 @@ rem execute PS1 script.
 powershell -executionpolicy bypass -file "%script_directory%%script_name%"
 
 rem pause, press any key to exit.
-=======
-@echo off
-rem 2020-11-14 Sukri Created.
-
-rem script name.
-set "script_name=Install-7Zip.ps1"
-
-rem script directory.
-set "script_directory=%~dp0"
-
-rem execute PS1 script.
-powershell -executionpolicy bypass -file "%script_directory%%script_name%"
-
-rem pause, press any key to exit.
->>>>>>> .theirs
 pause
